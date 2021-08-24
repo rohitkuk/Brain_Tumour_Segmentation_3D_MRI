28d2ccea88b3efd334be41809eb39be54e8fcec3
rohitkuk <rohit.kukreja01@gmail.com>
2021-08-24 18:07:47 +0530

    loader error

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ BATCH_SIZE = 1
 training_set = Brats2020Dataset2020(root = ROOT, train=True, transform=None , download=True)
 validation_set = Brats2020Dataset2020(root = ROOT, train=False, transform=None , download=False)
 
-training_loader = DataLoader(train_dataset, batch_size = BATCH_SIZE, shuffle=True, drop_last=True)
+training_loader = DataLoader(training_set, batch_size = BATCH_SIZE, shuffle=True, drop_last=True)
 validation_loader = DataLoader(validation_set, batch_size = BATCH_SIZE, shuffle=True, drop_last=True)
 
 
