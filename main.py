cf466e3b649b4a585a051064c4e194c788ece6ee
rohitkuk <rohit.kukreja01@gmail.com>
2021-08-24 19:56:48 +0530

    num_samples_error

--- main.py
+++ main.py
@@ -33,12 +33,12 @@ BATCH_SIZE = 1
 training_set = Brats2020Dataset2020(root = ROOT, train=True, transform=None , download=True)
 validation_set = Brats2020Dataset2020(root = ROOT, train=False, transform=None , download=False)
 
-training_loader = DataLoader(training_set, batch_size = BATCH_SIZE, shuffle=True, drop_last=True)
-validation_loader = DataLoader(validation_set, batch_size = BATCH_SIZE, shuffle=True, drop_last=True)
+# training_loader = DataLoader(training_set, batch_size = BATCH_SIZE, shuffle=True, drop_last=True)
+# validation_loader = DataLoader(validation_set, batch_size = BATCH_SIZE, shuffle=True, drop_last=True)
 
 
-print(len(training_loader))
-print(len(Validation_loader))
+print(len(training_set))
+print(len(training_set))
 
 
 
--- src/data.py
+++ src/data.py
@@ -109,7 +109,9 @@ class Brats2020Dataset2020(Dataset):
         img, target =  nib.load(self.images_t1c[index]), nib.load(self.images_seg[index])
         
         img, target = img.get_fdata(), target.get_fdata()
+        print("here")
         print(img.shape)
+
         # target = ((target == 1) | (target == 4)).astype('float32')
 
         # if self.transform:
