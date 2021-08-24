fe80ee0967274a64a07db51d64994f1778d66802
rohitkuk <rohit.kukreja01@gmail.com>
2021-08-24 18:03:50 +0530

    data loaders initiated

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ ToDo:
 
 - Research - Done
 - Setup Directory Structre and Github - Done
-x Solve for Data Download, Extract and Process
-x Dataset Class 
+- Solve for Data Download, Extract and Process - Done
+- Dataset Class - Done
 x Build 3D U net Model
 x Defie Loss Function
 x Define Other Metrics
@@ -25,10 +25,24 @@ x Things to improve
 
 import os
 from src.data import Brats2020Dataset2020
+from torch.utils.data import DataLoader
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
+BATCH_SIZE = 1
 
 training_set = Brats2020Dataset2020(root = ROOT, train=True, transform=None , download=True)
+validation_set = Brats2020Dataset2020(root = ROOT, train=False, transform=None , download=False)
+
+training_loader = DataLoader(train_dataset, batch_size = BATCH_SIZE, shuffle=True, drop_last=True)
+validation_loader = DataLoader(validation_set, batch_size = BATCH_SIZE, shuffle=True, drop_last=True)
+
+
+print(len(training_loader))
+print(len(Validation_loader))
+
+
+
+
 
 
 
